654f396d666a8c2bdd77fa20903e70d6b4b91fc6
Kevin Colten <kevincolten@gmail.com>
2020-01-06 01:41:17 -0600

    build

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,12 @@ if [ "${CIRCLE_BRANCH}" == "preview" ] || [ "${CIRCLE_BRANCH}" == "master" ]; th
   yarn jekyll-build-amp
   yarn optimize
   yarn css
-  ITER=0
-  for file in ./_configs/*; do
-    if [[ -f $file ]] && [[ $(($ITER % $CIRCLE_NODE_TOTAL)) == $CIRCLE_NODE_INDEX ]]; then
-      export KEY=$(echo $file | sed "s/^.\/_configs\/\(.*\).yml$/\1/")
-      yarn jekyll-build
-      yarn favicon
-      if [ "${CIRCLE_BRANCH}" == "master" ]; then yarn sitemap; fi
-      yarn jekyll-build
-      yarn jekyll-build-amp
-      yarn optimize
-      if [ "${CIRCLE_BRANCH}" == "preview" ]; then yarn encrypt; fi
-      yarn deploy
-    fi
-    ((ITER++))
-  done
+  yarn jekyll-build
+  yarn favicon
+  if [ "${CIRCLE_BRANCH}" == "master" ]; then yarn sitemap; fi
+  yarn jekyll-build
+  yarn jekyll-build-amp
+  yarn optimize
+  if [ "${CIRCLE_BRANCH}" == "preview" ]; then yarn encrypt; fi
+  yarn deploy
 fi
